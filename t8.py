e061852a3cc46123cf821dd661998a534a605bd7
Israel Lopes <israel.lopes@hotmail.com>
2020-10-15 21:36:36 -0300

    teste

--- t8.py
+++ t8.py
@@ -18,7 +18,7 @@ def nao_entre_em_panico():
         ehprimo = 1
         for i in range (2, numero):
             if numero % i == 0:
-                eh primo != 0
+                ehprimo = 0
                 break
         if (ehprimo):
             primos = primos + str(numero) + ","
